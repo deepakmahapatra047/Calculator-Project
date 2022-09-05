9ed17515ab552fab64a97e50b505eebf6761d84e
Deepak Mahapatra <deepakmahapatra047@gmail.com>
2022-09-05 13:05:53 +0530

    edited

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 let num="";
 
 function entry(e){
-   
+    
+
+
+    let ans= document.getElementById("screen")
+    let btn=document.getElementsByTagName("button")[e]
+    console.log(btn.innerText)
+    num+=btn.innerText
+    ans.value=num
+
+
 }
 
 function clr(){
+    let ans= document.getElementById("screen")
+
+    num=""
+    ans.value=num;
     
 }
 function calculate(){
-    calculator.screen = eval(num);
+    if(num==""){
+        ans.value=""
+    }
+
+    let ans= document.getElementById("screen")
+
+   ans.value= eval(ans.value)
+
+
 }
+
